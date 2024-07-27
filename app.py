6bbcdcfd7442a6b8659389878fbcfdb6b4b6ff30
rigizer <rigizer@gmail.com>
2024-07-28 00:47:22 +0900

    Flask 프레임워크 대신 Quart 프레임워크 사용 (함수 비동기)

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask     # pip install flask
-from flask import request
+from quart import Quart, request  # pip install quart
 from datetime import datetime
 
 import os
-import telegram             # pip install python-telegram-bot
+import telegram  # pip install python-telegram-bot
 import asyncio
 import platform
 
@@ -14,37 +13,29 @@ SERVER_NAME = os.environ['SERVER_NAME']
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_CHATID = os.environ['TELEGRAM_CHATID']
 
-bot = telegram.Bot(token = TELEGRAM_TOKEN)
-app = Flask(__name__)
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+app = Quart(__name__)
 
 @app.route('/')
-def send_message():
+async def send_message():
     time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
 
     name: str = request.args.get('name')
     size: int = size_convert(int(request.args.get('size')))
     path: str = request.args.get('path')
 
-    content = '<b>[Qbittorrent]</b> 다운로드 완료\n[SERVER] {}\n\n이름: {}\n크기: {}\n저장경로 {}\n완료시간: {}'.format(SERVER_NAME, name, size, path, time)
+    content = '[Qbittorrent] 다운로드 완료\n[SERVER] {}\n\n이름: {}\n크기: {}\n저장경로 {}\n완료시간: {}'.format(SERVER_NAME, name, size, path, time)
     
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        loop.run_until_complete(bot.sendMessage(chat_id=TELEGRAM_CHATID, text=content, parse_mode='html'))
-    finally:
-        loop.close()
+    await bot.send_message(chat_id=TELEGRAM_CHATID, text=content)
     
     return 'success'
 
 def size_convert(size):
     if size >= 1024:
-        # 1024보다 큰 경우 KBytes 단위로 변환
         size = round(size / 1024, 2)
         if size >= 1024:
-            # 1024보다 큰 경우 MBytes 단위로 변환
             size = round(size / 1024, 2)
             if size >= 1024:
-                # 1024보다 큰 경우 GBytes 단위로 변환
                 size = round(size / 1024, 2)
                 size = "{} GBytes".format(size)
             else:
